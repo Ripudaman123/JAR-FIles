a6bbd1143fed6ac1752bceaccd3690b881c32296
Ripudaman123 <rubalsingh1222@gmail.com>
2018-08-04 20:32:50 +0530

    Add files via upload

---
+++ .gitignore/JAR File/src/Emp.java
@@ -0,0 +1,10 @@
+//Creating JAR file
+public class Emp {
+
+	public static void main(String[] args) {
+
+		System.out.println("EMPLOYEE");
+
+	}
+
+}
---
+++ .gitignore/JAR File/src/Pack2/Org.java
@@ -0,0 +1,11 @@
+package Pack2;
+//Creating a package pack2 and class Org and making its Jar file
+public class Org {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Org from Pack2 Accessed.");
+
+	}
+
+}
